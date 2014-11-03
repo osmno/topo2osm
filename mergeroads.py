7ce81f96791bad09c80fed22e5e0827b9beed253
Torstein I. Bø <torsteinibo@gmail.com>
2014-11-03 21:46:45 +0100

    Started cleaning

--- mergeroads.py
+++ mergeroads.py
@@ -1,5 +1,6 @@
 from lxml import etree
 from math import sin, cos, sqrt, atan2, radians
+import sys
 
 def nodes2nodeList(nodes):
     l = {}
@@ -109,7 +110,7 @@ def nearestNodeInWay(node,nodeListNewWay,wayCandidate,nodeListCandidate,minNode=
     assert shortestDistance < 1e100
     return {'node':iShortest, 'distance':shortestDistance}
 
-def nearestNodeInNewWay(node,nodeListCandidate,way,nodeListNewWay,minNode=0,minSegment=0,maxNode=-1,maxSegment=-1):
+def nearestNodeInNewWay(node,nodeListCandidate,way,nodeListNewWay):
     node = nodeListCandidate[node.attrib['ref']]
     lat = float(node.attrib['lat'])
     lon = float(node.attrib['lon'])
@@ -120,22 +121,94 @@ def nearestNodeInNewWay(node,nodeListCandidate,way,nodeListNewWay,minNode=0,minS
     for segment in way['ways']:
         segmentI += 1
         nodeJ = -1
-        if segmentI>=minSegment and (maxSegment <= 0 or segmentI <= maxSegment):
-            for n in segment.findall("nd"):
-                nodeJ += 1
-                if nodeJ>=minNode and (maxNode < 0 or nodeJ <= maxNode):
-                    n = nodeListNewWay[n.attrib['ref']]
-                    lat2 = float(n.attrib["lat"])
-                    lon2 = float(n.attrib["lon"])
-                    distance = latLonDistance(lon,lat,lon2,lat2)
-                    if distance < shortestDistance:
-                        shortestDistance = distance
-                        nodeShortest = nodeJ
-                        segmentShortest = segmentI
+        for n in segment.findall("nd"):
+            nodeJ += 1
+            n = nodeListNewWay[n.attrib['ref']]
+            lat2 = float(n.attrib["lat"])
+            lon2 = float(n.attrib["lon"])
+            distance = latLonDistance(lon,lat,lon2,lat2)
+            if distance < shortestDistance:
+                shortestDistance = distance
+                nodeShortest = nodeJ
+                segmentShortest = segmentI
     return {'segment':segmentShortest,'node':nodeShortest, 'distance':shortestDistance}
 
-new = etree.parse("new.osm")
-old = etree.parse("old.osm")
+def distanceBetweenWays(oldNodes,newWay,newNodes,nodesCandidate,cropStartCandidate,cropEndCandidate):        
+    # Find length to nodes in way
+    i = -1
+    distance = []
+    for node in nodesCandidate:
+        i += 1
+        if (i >= cropStartCandidate) and (cropEndCandidate < 0 or i<=cropEndCandidate):
+            out = nearestNodeInNewWay(node,oldNodes,newWay,newNodes)
+            distance.append(out['distance'])
+    # Find mean and variance of distance between roads
+    mean = 0.
+    for d in distance:
+        mean += d
+    mean /= float(len(distance))
+    variance = 0.
+    for d in distance:
+        variance += (d-mean)**2
+    variance /= float(len(distance))
+    variance = variance**.5
+    return (mean,variance)
+
+def findCropCandidate(nodesCandidate,oldNodes,newWay,newNodes):
+    # Find begining and end of road
+    beginingCandidate2newWay = nearestNodeInNewWay(nodesCandidate[0],oldNodes,newWay,newNodes)
+    endCandidate2newWay = nearestNodeInNewWay(nodesCandidate[-1],oldNodes,newWay,newNodes)
+    beginingNewWay2Candidate =  nearestNodeInWay(newWay['ways'][0].findall('nd')[0],newNodes,candidate,oldNodes)
+    endNewWay2Candidate =       nearestNodeInWay(newWay['ways'][-1].findall('nd')[-1],newNodes,candidate,oldNodes)
+    ## Check if candidate way should be reversed
+    cropStartCandidate = 0
+    cropEndCandidate = 0
+    # Check which begining is closest to the other road
+    if (beginingNewWay2Candidate['node']<endNewWay2Candidate['node']):
+        if beginingNewWay2Candidate['distance']<beginingCandidate2newWay['distance']:
+            cropStartCandidate = beginingNewWay2Candidate['node']
+        else:
+            cropStartCandidate = 0
+        if endNewWay2Candidate['distance']<endCandidate2newWay['distance']:
+            cropEndCandidate = endNewWay2Candidate['node']
+        else:
+            cropEndCandidate = -1
+                    
+    else:
+        if beginingNewWay2Candidate['distance']<endCandidate2newWay['distance']:
+            cropEndCandidate = beginingNewWay2Candidate['node']
+        else:
+            cropEndCandidate = -1
+        if endNewWay2Candidate['distance']<beginingCandidate2newWay['distance']:
+            cropStartCandidate = endNewWay2Candidate['node']
+        else:
+            cropStartCandidate = 0  
+    return cropStartCandidate, cropEndCandidate
+
+## remove delted nodes
+def removeNodesNotInWay():
+    # list all nodes
+    ref = set()
+    for i in newNodes:
+        ref.add(int(i))
+    for way in new.getroot().findall('way'):
+        for n in way.findall('nd'):
+            if int(n.attrib['ref']) in ref:
+                ref.remove(int(n.attrib['ref']))
+    for r in ref:
+        new.getroot().remove(newNodes[str(r)])
+
+if len(sys.argv) is not 4:
+    print """The script requires three inputs, %d was given
+Usage: python mergeroads new.osm old.osm output.osm
+- new.osm  Data to be merged into old.osm
+- old.osm Existing data, new ways should not be close to ways in old.osm
+- output.osm Output file
+Input was: %s """  % (len(sys.argv)-1, str(sys.argv))
+    exit()
+    
+new = etree.parse(sys.argv[1])
+old = etree.parse(sys.argv[2])
 
 newWays = new.findall("way")
 newNodes = nodes2nodeList(new.findall("node"))
@@ -147,94 +220,21 @@ setBBox(oldWays,oldNodes)
 newWays = combineRoads(newWays)
 for k,newWay in newWays.iteritems():
     # Find roads with overlapping bBox (Union)
-    ways = findCloseOverlappingRoads(newWay,oldWays)
-    smallestMean = 1e100
-    smallestVar = 1e100
-    for candidate in ways:
+    closeWays = findCloseOverlappingRoads(newWay,oldWays)
+    for candidate in closeWays:
         nodesCandidate = candidate.findall('nd')
-        n = nodesCandidate[0]
-        # Find begining and end of road
-        beginingCandidate2newWay = nearestNodeInNewWay(nodesCandidate[0],oldNodes,newWay,newNodes)
-        endCandidate2newWay = nearestNodeInNewWay(nodesCandidate[-1],oldNodes,newWay,newNodes)
-        beginingNewWay2Candidate =  nearestNodeInWay(newWay['ways'][0].findall('nd')[0],newNodes,candidate,oldNodes)
-        endNewWay2Candidate =       nearestNodeInWay(newWay['ways'][-1].findall('nd')[-1],newNodes,candidate,oldNodes)
-        # Check if candidate way should be reversed
-        cropStartCandidate = 0
-        cropEndCandidate = 0
-        cropStartNewWay = {'node':0,'segment':0}
-        cropEndNewWay = {'node':0,'segment':0}
-        # Check which begining is closest to the other road
-        if (beginingNewWay2Candidate['node']<endNewWay2Candidate['node']):
-            if beginingNewWay2Candidate['distance']<beginingCandidate2newWay['distance']:
-                cropStartNewWay = {'node':0,'segment':0}
-                cropStartCandidate = beginingNewWay2Candidate['node']
-            else:
-                cropStartNewWay = {'node':beginingCandidate2newWay['node'],'segment':beginingCandidate2newWay['segment']}
-                cropStartCandidate = 0
-            if endNewWay2Candidate['distance']<endCandidate2newWay['distance']:
-                cropEndCandidate = endNewWay2Candidate['node']
-                cropEndNewWay = {'node':-1,'segment':-1}
-            else:
-                cropEndCandidate = -1
-                cropEndNewWay = {'node':endCandidate2newWay['node'],'segment':endCandidate2newWay['segment']}
-                        
-        else:
-            if beginingNewWay2Candidate['distance']<endCandidate2newWay['distance']:
-                cropStartNewWay = {'node':0,'segment':0}
-                cropEndCandidate = beginingNewWay2Candidate['node']
-            else:
-                cropStartNewWay = {'node':endCandidate2newWay['node'],'segment':endCandidate2newWay['segment']}
-                cropEndCandidate = -1
-            if endNewWay2Candidate['distance']<beginingCandidate2newWay['distance']:
-                cropStartCandidate = endNewWay2Candidate['node']
-                cropEndNewWay = {'node':-1,'segment':-1}
-            else:
-                cropStartCandidate = 0
-                cropEndNewWay = {'node':beginingCandidate2newWay['node'],'segment':beginingCandidate2newWay['segment']}
         
-        # Find length to nodes in way
-        i = -1
-        distance = []
-        for node in nodesCandidate:
-            i += 1
-            if (i >= cropStartCandidate) and (cropEndCandidate < 0 or i<=cropEndCandidate):
-                out = nearestNodeInNewWay(node,oldNodes,newWay,newNodes,cropStartNewWay['node'],cropStartNewWay['segment'],cropEndNewWay['node'],cropEndNewWay['segment'])
-                distance.append(out['distance'])
-        #print distance
-        # Find mean and variance of distance between roads
-        mean = 0.
-        for d in distance:
-            mean += d
-        mean /= float(len(distance))
-        variance = 0.
-        for d in distance:
-            variance += (d-mean)**2
-        variance /= float(len(distance))
-        variance = variance**.5
-        smallestMean = min(mean,smallestMean)
-        smallestVar = min(variance,smallestVar)
-        if mean < 50 and variance < 50**2:
+        cropStartCandidate, cropEndCandidate = findCropCandidate(nodesCandidate,oldNodes,newWay,newNodes)
+        mean, variance = distanceBetweenWays(oldNodes,newWay,newNodes,nodesCandidate,cropStartCandidate,cropEndCandidate)
+        if mean < 20 and variance < 5**2:
             # newWay is in oldWays if mean<tolMean and var<tolVar
             for way in newWay['ways']:
                 new.getroot().remove(way)
             break
-        else:
-            for way in newWay['ways']:
-                way.attrib['mean']=str(smallestMean)
-                way.attrib['var'] = str(smallestVar)
-
-## remove delted nodes
-# list all nodes
-ref = set()
-for i in newNodes:
-    ref.add(int(i))
-for way in new.getroot().findall('way'):
-    for n in way.findall('nd'):
-        if int(n.attrib['ref']) in ref:
-            ref.remove(int(n.attrib['ref']))
-for r in ref:
-    new.getroot().remove(newNodes[str(r)])
 
 
-new.write("testParsed.osm")
+
+
+removeNodesNotInWay()
+new.write(sys.argv[3])
 
